f01fc7d14ddf6f939a2ece3a4bd919bdc13255f6
Sangam Kumar <132322216+sangam2256@users.noreply.github.com>
2024-06-18 15:03:50 +0530

    PROJECT_Solidity.sol

--- PROJECT_Solidity.sol
+++ PROJECT_Solidity.sol
@@ -7,7 +7,7 @@ pragma solidity 0.8.7;
     2. Your contract will have a mapping of addresses to balances (address => uint)
     3. You will have a mint function that takes two parameters: an address and a value. 
        The function then increases the total supply by that number and increases the balance 
-       of the “sender” address by that amount
+       of the “sender” address by that amount.
     4. Your contract will have a burn function, which works the opposite of the mint function, as it will destroy tokens. 
        It will take an address and value just like the mint functions. It will then deduct the value from the total supply 
        and from the balance of the “sender”.
